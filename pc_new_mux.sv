e4f54a0fb8068b3ff15ff56b895d8a7f226d3d65
mosahel <sahel.70444@gmail.com>
2026-02-18 16:18:54 +0530

    Delete pc_new_mux.sv

--- pc_new_mux.sv
+++
@@ -1,9 +0,0 @@
-module pc_new_mux(
-                  input logic[15:0] pc_plus4,
-                  input logic[15:0] dest_pc,
-                  input logic pc_src,
-                  output logic[15:0] pc_new
-                  );
-                  
-                 assign pc_new = (pc_src)?dest_pc : pc_plus4;
-endmodule
