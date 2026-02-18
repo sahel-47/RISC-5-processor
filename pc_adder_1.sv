0267dbcc059153e14a1d18a8163f28f33e4fb607
mosahel <sahel.70444@gmail.com>
2026-02-18 16:19:02 +0530

    Delete pc_adder_1.sv

--- pc_adder_1.sv
+++
@@ -1,7 +0,0 @@
-module pc_adder_1(
-                  input logic[15:0] pc,
-                  output logic[15:0] pc_plus4
-                  );
-                  
-                  assign pc_plus4 = pc + 16'b0000_0000_0000_0100;
-endmodule
